cdd1e98594ef89a0c61e3784793c1bf6697a034b
Toni Nystrom <toni.nystrom@inuit.fi>
2016-11-25 21:45:45 +0200

    photo from api YAY

---
+++ test.js
@@ -0,0 +1,254 @@
+{
+  "id" : "TA111B0Q9-G11",
+  "modelId" : "TA111B0Q9",
+  "name" : "Platform heels - chili ",
+  "shopUrl" : "https://www.zalando.co.uk/tamaris-platform-heels-chili-ta111b0q9-g11.html",
+  "color" : "Red",
+  "available" : true,
+  "season" : "WINTER",
+  "seasonYear" : "2016",
+  "activationDate" : "2016-06-06T16:54:15+02:00",
+  "additionalInfos" : [ ],
+  "tags" : [ ],
+  "genders" : [ "FEMALE" ],
+  "ageGroups" : [ "ADULT" ],
+  "brand" : {
+    "key" : "TA1",
+    "name" : "Tamaris",
+    "logoUrl" : "https://i2.ztat.net/brand/tamaris.jpg",
+    "logoLargeUrl" : "https://i2.ztat.net/brandxl/tamaris.jpg",
+    "shopUrl" : "https://www.zalando.co.uk/tamaris"
+  },
+  "categoryKeys" : [ "catalog", "women", "womens-shoes", "womens-shoes-heels", "platform-heels", "womens-shoes-high-heels", "womens-sale", "womens-shoes-sale", "womens-heels-sale", "womens-platform-heels-sale", "womens-high-heels-sale", "all", "shoes", "sale" ],
+  "attributes" : [ {
+    "name" : "upper material",
+    "values" : [ "high-quality imitation leather" ]
+  }, {
+    "name" : "shoe toecap",
+    "values" : [ "round" ]
+  }, {
+    "name" : "shoe fastener",
+    "values" : [ "slip on" ]
+  }, {
+    "name" : "Fabric",
+    "values" : [ "Synthetic leather" ]
+  }, {
+    "name" : "Platform height",
+    "values" : [ "0.5 \" (Size 4)" ]
+  }, {
+    "name" : "Cover sole",
+    "values" : [ "imitation leather" ]
+  }, {
+    "name" : "Pattern",
+    "values" : [ "plain" ]
+  }, {
+    "name" : "Padding type",
+    "values" : [ "Cold padding" ]
+  }, {
+    "name" : "Heel type",
+    "values" : [ "stiletto", "platform toe" ]
+  }, {
+    "name" : "Sole",
+    "values" : [ "synthetics" ]
+  }, {
+    "name" : "heel height",
+    "values" : [ "3.5 \" (Size 4)" ]
+  }, {
+    "name" : "Internal material",
+    "values" : [ "textile" ]
+  } ],
+  "units" : [ {
+    "id" : "TA111B0Q9-G110410000",
+    "size" : "8",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : true,
+    "stock" : 1
+  }, {
+    "id" : "TA111B0Q9-G110350000",
+    "size" : "2",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : true,
+    "stock" : 3
+  }, {
+    "id" : "TA111B0Q9-G110400000",
+    "size" : "7",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : false,
+    "stock" : 0
+  }, {
+    "id" : "TA111B0Q9-G110380000",
+    "size" : "5",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : true,
+    "stock" : 3
+  }, {
+    "id" : "TA111B0Q9-G110420000",
+    "size" : "9",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : false,
+    "stock" : 0
+  }, {
+    "id" : "TA111B0Q9-G110390000",
+    "size" : "6",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : true,
+    "stock" : 3
+  }, {
+    "id" : "TA111B0Q9-G110370000",
+    "size" : "4",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : true,
+    "stock" : 3
+  }, {
+    "id" : "TA111B0Q9-G110360000",
+    "size" : "3",
+    "price" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "originalPrice" : {
+      "currency" : "GBP",
+      "value" : 39.99,
+      "formatted" : "£39.99"
+    },
+    "available" : true,
+    "stock" : 3
+  } ],
+  "media" : {
+    "images" : [ {
+      "orderNumber" : 1,
+      "type" : "NON_MODEL",
+      "thumbnailHdUrl" : "https://i6.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg",
+      "smallUrl" : "https://i6.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg",
+      "smallHdUrl" : "https://i6.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg",
+      "mediumUrl" : "https://i6.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg",
+      "mediumHdUrl" : "https://i6.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg",
+      "largeUrl" : "https://i6.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg",
+      "largeHdUrl" : "https://i6.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@11.5.jpg"
+    }, {
+      "orderNumber" : 2,
+      "type" : "PREMIUM",
+      "thumbnailHdUrl" : "https://i2.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg",
+      "smallUrl" : "https://i2.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg",
+      "smallHdUrl" : "https://i2.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg",
+      "mediumUrl" : "https://i2.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg",
+      "mediumHdUrl" : "https://i2.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg",
+      "largeUrl" : "https://i2.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg",
+      "largeHdUrl" : "https://i2.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@12.4.jpg"
+    }, {
+      "orderNumber" : 3,
+      "type" : "NON_MODEL",
+      "thumbnailHdUrl" : "https://i4.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg",
+      "smallUrl" : "https://i4.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg",
+      "smallHdUrl" : "https://i4.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg",
+      "mediumUrl" : "https://i4.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg",
+      "mediumHdUrl" : "https://i4.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg",
+      "largeUrl" : "https://i4.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg",
+      "largeHdUrl" : "https://i4.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@9.5.jpg"
+    }, {
+      "orderNumber" : 4,
+      "type" : "NON_MODEL",
+      "thumbnailHdUrl" : "https://i5.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg",
+      "smallUrl" : "https://i5.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg",
+      "smallHdUrl" : "https://i5.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg",
+      "mediumUrl" : "https://i5.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg",
+      "mediumHdUrl" : "https://i5.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg",
+      "largeUrl" : "https://i5.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg",
+      "largeHdUrl" : "https://i5.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@10.5.jpg"
+    }, {
+      "orderNumber" : 5,
+      "type" : "NON_MODEL",
+      "thumbnailHdUrl" : "https://i6.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg",
+      "smallUrl" : "https://i6.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg",
+      "smallHdUrl" : "https://i6.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg",
+      "mediumUrl" : "https://i6.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg",
+      "mediumHdUrl" : "https://i6.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg",
+      "largeUrl" : "https://i6.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg",
+      "largeHdUrl" : "https://i6.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@5.5.jpg"
+    }, {
+      "orderNumber" : 6,
+      "type" : "NON_MODEL",
+      "thumbnailHdUrl" : "https://i3.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg",
+      "smallUrl" : "https://i3.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg",
+      "smallHdUrl" : "https://i3.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg",
+      "mediumUrl" : "https://i3.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg",
+      "mediumHdUrl" : "https://i3.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg",
+      "largeUrl" : "https://i3.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg",
+      "largeHdUrl" : "https://i3.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@8.5.jpg"
+    }, {
+      "orderNumber" : 7,
+      "type" : "NON_MODEL",
+      "thumbnailHdUrl" : "https://i2.ztat.net/thumb_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg",
+      "smallUrl" : "https://i2.ztat.net/catalog/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg",
+      "smallHdUrl" : "https://i2.ztat.net/catalog_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg",
+      "mediumUrl" : "https://i2.ztat.net/detail/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg",
+      "mediumHdUrl" : "https://i2.ztat.net/detail_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg",
+      "largeUrl" : "https://i2.ztat.net/large/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg",
+      "largeHdUrl" : "https://i2.ztat.net/large_hd/TA/11/1B/0Q/9G/11/TA111B0Q9-G11@7.5.jpg"
+    } ]
+  }
+}
